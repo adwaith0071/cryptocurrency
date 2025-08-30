353a97cb8a5cc0e057327681ad3c9e794ab52322
Adwaith B <adwaithb058@gmail.com>
2025-08-30 10:16:22 +0530

    update

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -43,24 +43,24 @@ export const HeroSection: React.FC = () => {
       </div>
 
 
-      <div className="hidden lg:block absolute top-32 left-32 w-16 h-16 bg-[#fee71533] rounded-full flex items-center justify-center">
+      <div className="absolute top-2 left-8 md:top-8 md:left-16 lg:top-20 lg:left-32 w-12 h-12 md:w-16 md:h-16 lg:w-16 lg:h-16 bg-[#fee71533] rounded-full flex items-center justify-center">
         <div className="flex items-center justify-center w-full h-full">
-          <img className="w-8 h-8 object-contain" alt="Bitcoin" src="/coins-2--5.png" />
+          <img className="w-6 h-6 md:w-8 md:h-8 lg:w-8 lg:h-8 object-contain" alt="Bitcoin" src="/coins-2--5.png" />
         </div>
       </div>
-      <div className="hidden lg:block absolute top-28 right-28 w-20 h-20 bg-[#cc313d33] rounded-full flex items-center justify-center rotate-12">
+      <div className="absolute top-0 right-6 md:top-12 md:right-20 lg:top-16 lg:right-28 w-14 h-14 md:w-18 md:h-18 lg:w-20 lg:h-20 bg-[#cc313d33] rounded-full flex items-center justify-center rotate-12">
         <div className="flex items-center justify-center w-full h-full">
-          <img className="w-10 h-10 object-contain" alt="Doge" src="/coins-2--5-3.png" />
+          <img className="w-7 h-7 md:w-9 md:h-9 lg:w-10 lg:h-10 object-contain" alt="Doge" src="/coins-2--5-3.png" />
         </div>
       </div>
-      <div className="hidden xl:block absolute bottom-40 left-40 w-14 h-14 bg-[#5928af33] rounded-full flex items-center justify-center -rotate-12">
+      <div className="absolute bottom-20 left-8 md:bottom-32 md:left-20 lg:bottom-40 lg:left-40 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-[#5928af33] rounded-full flex items-center justify-center -rotate-12">
         <div className="flex items-center justify-center w-full h-full">
-          <img className="w-7 h-7 object-contain" alt="Ethereum" src="/coins-2--5-1.png" />
+          <img className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 object-contain" alt="Ethereum" src="/coins-2--5-1.png" />
         </div>
       </div>
-      <div className="hidden xl:block absolute bottom-32 right-40 w-12 h-12 bg-[#33875a33] rounded-full flex items-center justify-center rotate-45">
+      <div className="absolute bottom-16 right-8 md:bottom-24 md:right-16 lg:bottom-32 lg:right-40 w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 bg-[#33875a33] rounded-full flex items-center justify-center rotate-45">
         <div className="flex items-center justify-center w-full h-full">
-          <img className="w-6 h-6 object-contain" alt="Solana" src="/coins-2--5-2.png" />
+          <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6 object-contain" alt="Solana" src="/coins-2--5-2.png" />
         </div>
       </div>
     </section>
